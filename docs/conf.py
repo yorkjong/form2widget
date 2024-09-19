8084e5816c6c1697c6fd35fc02d558b27db453aa
York Jong <yukuan.jiang@gmail.com>
2024-09-20 02:10:53 +0800

    docs: Fix conf.py and change html_theme

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,6 @@ extensions = [
     'myst_parser'
 ]
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
@@ -52,5 +50,5 @@ source_suffix = ['.rst', '.md']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']
