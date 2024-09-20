210b2efa13f31a062f27b232964f78b094d37af4
York Jong <yukuan.jiang@gmail.com>
2024-09-20 08:38:13 +0800

    refactor: Update __init__.py to reflect function and module renaming

--- form2widget/__init__.py
+++ form2widget/__init__.py
@@ -7,20 +7,14 @@ __author__ = "York <york.jong@gmail.com>"
 __date__ = "2024/09/19 (initial version) ~ 2024/09/19 (last revision)"
 
 __all__ = [
-    'convert_to_widgets',
-    'insert_title_cells',
-    'remove_open_in_colab_cell',
-    'extract_and_comment_first_code_cell',
-    'search_section',
-    'extract_section',
-    'remove_section',
-    'run_code_cells',
-    'run_code_cell',
+    'widgetify',
+    'nb_op',
+    'nb_exec',
     'create_input_dropdown',
 ]
 
-from .colab_form_converter import *
-from .nb_op import *
-from .nb_exec import *
+from .form2widget import *
+from . import nb_op
+from . import nb_exec
 from .input_dropdown import *
 
